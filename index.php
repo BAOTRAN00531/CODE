2abbdc4b45424419e1512652ec1fbea860975a41
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2023-12-07 13:46:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -181,10 +181,10 @@
                                      $name=$_POST['name'];
                                      $email=$_POST['email'];
                                      $phone=$_POST['tel'];
-                                     $iduser=$_POST['iduser'];
                                      $pttt=1;
                                      $tinhtrang=1;
                                      $address=$_POST['address'];
+                                     $iduser=$_POST['iduser'];
                                      $ngaydathang=date('d-m-Y H:i:s');
                                      $tong=tongdon();
                                      $iddh=insert_giohang($iduser,$name, $address, $phone,$email, $tong, $pttt, $ngaydathang);
