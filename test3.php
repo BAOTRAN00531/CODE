1229b8e342ced8fd62e767c368fa36a53d6351a9
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2023-12-08 02:20:19 +0700

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,6 +1,20 @@
 <?php 
 include_once("dao/binhluan.php");
-
-binhluan_insert("33","16","aaaaa","8");
+$noidung = $_POST['noidung'];
+$idsp = $_POST['idsp'];
+$iduser = $_POST['iduser'];
+$ngay_bl = date('d-m-Y H:i:s');
+binhluan_insert( $iduser, $idsp, $noidung , $ngay_bl );
 echo "a";
 ?>
+                <form action="index.php?action=binhluan" method="post">
+                    <input type="hidden" name="idsp" id="idsp" value="<?=$idsp?>">
+                    <input type="hidden" name="iduser" id="iduser" value="<?=$iduser?>">
+                    <textarea name="noidung" cols="70" row="9"
+                        placeholder="FutureBooks xin đánh giá của quý khách..."></textarea>
+                    <div class="btn-group">
+                        <button type="submit" name="guibinhluan" class="btn submit">Submit</button>
+                        <button onclick="cancel" class="btn cancel">Cancel</button>
+
+                    </div>
+                </form>
