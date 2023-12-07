a6f34a479205f17fa2844a157b4612cdea8fd42d
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2023-12-08 02:23:43 +0700

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,6 +1,6 @@
 <?php 
 include_once("dao/binhluan.php");
-if ( isset($_GET["action"]) && ($_GET["action"])) {
+if ( isset($_GET["actiona"]) && ($_GET["actiona"])) {
 $noidung = $_POST['noidung'];
 $idsp = $_POST['idsp'];
 $iduser = $_POST['iduser'];
@@ -9,7 +9,7 @@ binhluan_insert( $iduser, $idsp, $noidung , $ngay_bl );
 echo "a";
 }
 ?>
-                <form action="action=a" method="post">
+                <form action="actiona=a" method="post">
                     <input type="hidden" name="idsp" id="idsp" value="<?=$idsp?>">
                     <input type="hidden" name="iduser" id="iduser" value="<?=$iduser?>">
                     <textarea name="noidung" cols="70" row="9"
