7576621566206a2a0b08996d0ef89c034f65850c
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2023-12-08 03:05:51 +0700

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,21 +1,21 @@
 <?php 
-include_once("dao/binhluan.php");
-if ( isset($_GET["a"]) && ($_GET["a"])) {
-    $a = $_GET['a'];
-    switch ($a) {
-        case 'a':
-$noidung = $_POST['noidung'];
-$idsp = $_POST['idsp'];
-$iduser = $_POST['iduser'];
-$ngay_bl = date('d-m-Y H:i:s');
-binhluan_insert( $iduser, $idsp, $noidung , $ngay_bl );
-echo "a";
-break;
-        default:
-        echo "b";
-        break;
-        }
-}
+// include_once("dao/binhluan.php");
+// if ( isset($_GET["a"]) && ($_GET["a"])) {
+//     $a = $_GET['a'];
+//     switch ($a) {
+//         case 'a':
+// $noidung = $_POST['noidung'];
+// $idsp = $_POST['idsp'];
+// $iduser = $_POST['iduser'];
+// $ngay_bl = date('d-m-Y H:i:s');
+// binhluan_insert( $iduser, $idsp, $noidung , $ngay_bl );
+// echo "a";
+// break;
+//         default:
+//         echo "b";
+//         break;
+//         }
+// }
 ?>
                 <form action="<?php binhluan_insert($iduser,$idsp,"a","9") ?>" method="post">
                     <input type="hidden" name="idsp" id="idsp" value="<?=$idsp?>">
