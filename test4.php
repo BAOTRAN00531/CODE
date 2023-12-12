e9ed808ee63c2dcf41d9d56e3c783cd67495c4c1
BAOTRAN00531 <150698874+BAOTRAN00531@users.noreply.github.com>
2023-12-12 07:45:43 +0700

    dangnhap

---
+++ test4.php
@@ -0,0 +1,21 @@
+<table>
+  <tr>
+    <td colspan="2">ID:</td>
+  </tr>
+  <tr>
+    <td>Tên:</td>
+    <td><input type="text" name="name"></td>
+  </tr>
+  <tr>
+    <td>Tên đăng nhập:</td>
+    <td><label>$user</label></td>
+  </tr>
+  <tr>
+    <td>Tuổi:</td>
+    <td><input type="text" name="age"></td>
+  </tr>
+  <tr>
+    <td>Số điện thoại:</td>
+    <td><input type="text" name="phone"></td>
+  </tr>
+</table>
--- user.php
+++ user.php
@@ -12,23 +12,41 @@ $age = $v['TUOI'];
 $gender = $v['GIOITINH'];
 $phone = $v['SDT'];
 echo "
-<div align='center' style='width:50%;margin-left:300px'>
-<form action='index.php?action=edituser' method='post'>
-
-<label style='color:white' for='name'>ID</label><span><input type='text' name='iduser' readonly value='$iduser'></span> <br>
-<label style='color:white' for='name'>Ten</label>
-<input type='text' name='name' value='$name'> <br>
-<label style='color:white' for='user'>Ten dang nhap</label>
-<input type='text' name='user' readonly value='$user'> <br>
-<label style='color:white' for='age'>Tuoi</label>
-<input type='number' name='age' value ='$age'> <br>
-<label style='color:white' for='gender'>Gioi tinh</label>
-<input type='text' name='gender' value='$gender'> <br>
-<label style='color:white' for='phone'>SDT</label>
-<input type='number' name='phone' value='$phone'> <br>
+<div  style='width:86%;margin-left:37px;margin-top:50px'>
+<form  action='index.php?action=edituser' method='post'>
+<table >
+    <tr>
+        <td style = 'padding: 0px 20px 30px 0px;text-align: right;'><label style='color:white;' for='name'>ID:</label></td>
+        <td style = 'padding: 0px 0px 35px 20px;text-align: left;><label style='color:white'>$iduser </label> </td>
+        <td rowspan='5' style = 'width:400px;padding: 0px 0px 0px 20px;text-align: left;><label style='color:white'>$iduser </label> </td>
+    </tr>
+    <tr>
+    <td style = 'padding: 0px 20px 30px 0px;text-align: right;'><label style='color:white; for='name'>TEN:</label></td>
+    <td style = 'padding: 0px 0px 35px 20px;text-align: left;'><input style='width: fit-content;' type='text' name='name' value='$name'> </td>
+    </tr>
+    <tr>
+    <td style = 'padding: 0px 20px 30px 0px;text-align: right;'><label style='color:white;' for='name'>TEN DANG NHAP:</label></td>
+    <td style = 'padding: 0px 0px 35px 20px;text-align: left;'><label style='color:white'>$user </label> </td>
+    </tr>
+    <tr>
+    <td style = 'padding: 0px 20px 30px 0px;text-align: right;'><label style='color:white;' for='name'>TUOI:</label></td>
+    <td style = 'padding: 0px 0px 35px 20px;text-align: left;'><input style='width: fit-content;' type='text' name='name' value='$age'> </td>
+    </tr>
+    <tr>
+        <td style = 'padding: 0px 20px 30px 0px;text-align: right;'><label style='color:white;' for='name'>SODT:</label></td>
+        <td style = 'padding: 0px 0px 35px 20px;text-align: left;'><input style='width: fit-content;' type='text' name='name' value='$phone'> </td>
+    </tr>
+    
+</table>
 <input type='submit' name='edit'>
 </form>
+
+
 </div>
 ";
 }
 ?>
+<label for="" style=""></label>
+
+
+
